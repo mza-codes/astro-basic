4485c8b055300c6765c7f985e3ba786160cb65af
mza-codes <muzofficial90@gmail.com>
2023-05-02 13:08:31 +0530

    fix: build #3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
+
 export default defineConfig({
     integrations: [react(), tailwind()],
-    output: "static",
+    output: "server",
     adapter: vercel(),
 });
